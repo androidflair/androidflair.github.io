647548e482c59254b8cfef510a084d6441fc800a
nty <reddit.android@gmail.com>
2014-08-05 15:42:12 -0400

    small changes

--- thescript.js
+++ thescript.js
@@ -26,16 +26,13 @@ $('.ripple').on('mousedown', function(event) {
    proof = $("#proof").val();
   flairtext =  $("#flairtext").val();
   category = $("#entry_0").val();
-   $("#name").val(category);
   
   
+      var toopen = "%23Flair%20Application%3A%0A%0A--------------%0A%0A*Company%20Name%3A*%0A%0A%3E" + name + "%0A%0A*Requested%20Flair%20Text%3A*%0A%0A%3E" + flairtext + "%0A%0A*Flair%20Category%3A*%0A%0A%3E" + category + "%0A%0A*Proof%3A*%0A%0A%3E" + proof + "%0A%0A";
+
+      window.open("http://www.reddit.com/message/compose?to=%2Fr%2FAndroid&message=" + toopen);
   
-  
-  var toopen = "%23Flair%20Application%3A%0A%0A--------------%0A%0A*Company%20Name%3A*%0A%0A%3E" + name + "%0A%0A*Requested%20Flair%20Text%3A*%0A%0A%3E" + flairtext + "%0A%0A*Flair%20Category%3A*%0A%0A%3E" + category + "%0A%0A*Proof%3A*%0A%0A%3E" + proof + "%0A%0A";
-  
-  window.open("http://www.reddit.com/message/compose?to=%2Fr%2FAndroid&message=" + toopen);
-  
-  
+    
   
 });
 
