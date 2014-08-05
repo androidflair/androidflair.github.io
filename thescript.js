76d86d22d390629a8430c87e96b57423a8870272
nty <reddit.android@gmail.com>
2014-08-05 19:36:51 -0400

    url changes

--- thescript.js
+++ thescript.js
@@ -35,7 +35,7 @@ $('.ripple').on('mousedown', function(event) {
       alert("One or more field has been left blank.");
   }
     else{
-      var toopen = "%23Flair%20Application%3A%0A%0A--------------%0A%0A**Company%20Name%3A**%0A%0A%3E" + name + "%0A%0A**Requested%20Flair%20Text%3A**%0A%0A%3E" + flairtext + "%0A%0A*Flair%20Category%3A*%0A%0A%3E" + category + "%0A%0A**Proof%3A**%0A%0A%3E" + proof + "%0A%0A**Additional%20Information**%0A%0A%3E" + encodeURIComponent(additionalinfo);
+      var toopen = "%23Flair%20Application%3A%0A%0A--------------%0A%0A**Company%20Name%3A**%0A%0A%3E" + name + "%0A%0A**Requested%20Flair%20Text%3A**%0A%0A%3E" + flairtext + "%0A%0A**Flair%20Category%3A**%0A%0A%3E" + category + "%0A%0A**Proof%3A**%0A%0A%3E" + proof + "%0A%0A**Additional%20Information**%0A%0A%3E" + encodeURIComponent(additionalinfo);
 
       window.open("http://www.reddit.com/message/compose?to=%2Fr%2FrAndroidCSS&subject=Flair%20Application&message=" + toopen);
   
